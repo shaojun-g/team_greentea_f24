941510af6fcdb29eaf86b767a0e3a31c8de658f8
kwan-c <kwan.c@digipen.edu>
2024-10-23 10:01:58 +0800

    test

--- Gun N Hook/game.c
+++ Gun N Hook/game.c
@@ -14,6 +14,10 @@ void Game_Update(void)
 
 	CP_Graphics_ClearBackground(CP_Color_Create(100, 100, 100, 255));
 
+	if (CP_Input_KeyTriggered(KEY_Q))
+	{
+		CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
+	}
 }
 
 void Game_Exit(void)
