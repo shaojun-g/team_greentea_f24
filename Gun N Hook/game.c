af75e8fbf0810dfd0d1f7ec3b5503908046d2b87
shao jun <shaojun.g@digipen.edu>
2024-10-23 09:10:54 +0800

    Initial Commit

---
+++ Gun N Hook/game.c
@@ -0,0 +1,22 @@
+#include "cprocessing.h"
+#include <stdio.h>;
+#include "utils.h";
+#include "mainmenu.h";
+
+
+void Game_Init(void)
+{
+}
+
+
+void Game_Update(void)
+{
+
+	CP_Graphics_ClearBackground(CP_Color_Create(100, 100, 100, 255));
+
+}
+
+void Game_Exit(void)
+{
+
+}
